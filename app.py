7d415dba526cdd48088e6e502c6106b46eb3da31
DAVID LA PUMA <david@woodcreeper.com>
2025-05-06 14:16:08 -0400

    Use PORT env variable for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import folium
+import os
 from folium import Popup
 from folium.plugins import MousePosition
 from shapely.geometry import LineString
@@ -74,4 +75,5 @@ def index():
     """
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
